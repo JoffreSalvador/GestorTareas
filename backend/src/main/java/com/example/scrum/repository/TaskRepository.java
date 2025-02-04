73812c1fbd41cde0750250bd5ff11a5097a8fd5d
Joffre Salvador <joffre.salvador@epn.edu.ec>
2025-02-04 16:43:22 -0500

    Eliminar archivos de otra rama

--- backend/src/main/java/com/example/scrum/repository/TaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.scrum.repository;
-
-import com.example.scrum.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {}
